bdbc6e45962cc0b224425f2b4a516cd3d7e54de4
Abby <abigailsy1@yahoo.com>
2018-10-16 19:38:13 -0700

    created container folder and updated index

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './rootreducers';
 import App from './app';
+import promiseMiddleware from 'redux-promise-middleware';
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+
+const store = createStore( reducers, composeEnhancers (
+  applyMiddleware (
+    promiseMiddleware()
+  )
+));
+// const store = createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 /* eslint-enable */
+
 
 
 render(
